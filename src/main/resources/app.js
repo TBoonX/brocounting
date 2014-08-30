0eb5d7d846ec2f267fbbf36219e8975905f85a61
TBoonX <tboonx@googlemail.com>
2014-08-30 18:44:37 +0200

    set all routes

--- src/main/resources/app.js
+++ src/main/resources/app.js
@@ -11,9 +11,33 @@ app.config(['$routeProvider',
         templateUrl: 'partials/login.html',
         controller: 'LoginCtrl'
       }).
+      when('/registration', {
+        templateUrl: 'partials/registration.html',
+        controller: 'RegistrationCtrl'
+      })
       when('/transaction/:transactionId', {
         templateUrl: 'partials/transaction.html',
         controller: 'TransactionCtrl'
+      }).
+      when('/tags, {
+        templateUrl: 'partials/tags.html',
+        controller: 'TagsCtrl'
+      }).
+      when('/tag/:tagId', {
+        templateUrl: 'partials/tag.html',
+        controller: 'TagCtrl'
+      }).
+      when('/statistic', {
+        templateUrl: 'partials/statistic.html',
+        controller: 'StatisticCtrl'
+      }).
+      when('/accounts', {
+        templateUrl: 'partials/accounts.html',
+        controller: 'AccountsCtrl'
+      }).
+      when('/account/:accountId', {
+        templateUrl: 'partials/account.html',
+        controller: 'AccountCtrl'
       });
   }]);
 
@@ -27,5 +51,29 @@ app.controller('LoginController', [ '$http', '$scope', function($http, $scope) {
 } ]);
 
 app.controller('TransactionController', [ '$http', '$scope', '$routeParams', function ($http, $scope, $routeParams) {
+
+} ]);
+
+app.controller('RegistrationCtrl', [ '$http', '$scope', '$routeParams', function ($http, $scope, $routeParams) {
+
+} ]);
+
+app.controller('TagsCtrl', [ '$http', '$scope', '$routeParams', function ($http, $scope, $routeParams) {
+
+} ]);
+
+app.controller('TagCtrl', [ '$http', '$scope', '$routeParams', function ($http, $scope, $routeParams) {
+
+} ]);
+
+app.controller('StatisticCtrl', [ '$http', '$scope', '$routeParams', function ($http, $scope, $routeParams) {
+
+} ]);
+
+app.controller('AccountsCtrl', [ '$http', '$scope', '$routeParams', function ($http, $scope, $routeParams) {
+
+} ]);
+
+app.controller('AccountCtrl', [ '$http', '$scope', '$routeParams', function ($http, $scope, $routeParams) {
 
 } ]);
