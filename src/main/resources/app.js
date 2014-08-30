931cdce39241b6f4db65e8551e853dc74b1431a8
TBoonX <tboonx@googlemail.com>
2014-08-30 18:29:40 +0200

    Skeleton nr 2

--- src/main/resources/app.js
+++ src/main/resources/app.js
@@ -1,4 +1,31 @@
+'use strict';
 
+//create module
+var app = angular.module('brocounting', [ 'ngRoute' ]);
 
+//set routes
+app.config(['$routeProvider',
+  function ($routeProvider) {
+    $routeProvider.
+      when('/login', {
+        templateUrl: 'partials/login.html',
+        controller: 'LoginCtrl'
+      }).
+      when('/transaction/:transactionId', {
+        templateUrl: 'partials/transaction.html',
+        controller: 'TransactionCtrl'
+      });
+  }]);
 
-var app = angular.module('main', []);
+//create controllers
+app.controller('MainController', [ '$http', function($http) {
+
+} ]);
+
+app.controller('LoginController', [ '$http', '$scope', function($http, $scope) {
+
+} ]);
+
+app.controller('TransactionController', [ '$http', '$scope', '$routeParams', function ($http, $scope, $routeParams) {
+
+} ]);
